9198ac200884a58616e207f7cd3c3bd33c8997f8
Soffcsr <soff.csr@gmail.com>
2020-04-26 23:17:59 -0300

    response return html

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
 http.createServer(function(req, res){
-    res.write('Hello world');
+    //writeHead permite mandar con la respuesta el estado con el que esta regresando
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write('<h1>Hello world</h1>');
     res.end();
 }).listen(3000, function(){
     console.log('servidor escuchando en el puerto 3000')
